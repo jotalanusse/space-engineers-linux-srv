97d1e230247459638a724c5315fa3c4cb58d10ac
Jota Lanusse <jotalanusse@gmail.com>
2022-04-19 17:21:16 -0300

    Added default settings

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
-docker volume create steamcmd_volume # Optional: Location of SteamCMD installation
+echo "The setup will delete any existing servers and saves, continue?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
+
+
+docker volume create steamcmd_volume
 
 docker stop steamcmd
 docker rm steamcmd
@@ -9,5 +18,7 @@ mkdir -p ./se-server/config
 
 docker run -it -v "steamcmd_volume:/home/steam/steamcmd" --name=steamcmd cm2network/steamcmd /bin/sh -c "./steamcmd.sh +force_install_dir ./bins +login anonymous +app_update 298740 +quit"
 docker cp steamcmd:home/steam/steamcmd/bins/ ./se-server/
+
+cp ./source/SpaceEngineers-Dedicated.cfg ./se-server/config/SpaceEngineers-Dedicated.cfg
 
 docker rm steamcmd
