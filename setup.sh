3548ef009a0c47060b1e1719c0457603c33ebbfb
Jota Lanusse <jotalanusse@gmail.com>
2022-04-19 18:12:41 -0300

    Working version with world

--- setup.sh
+++ setup.sh
@@ -13,12 +13,17 @@ docker stop steamcmd
 docker rm steamcmd
 
 rm -rf ./se-server
+
 mkdir -p ./se-server/bins
 mkdir -p ./se-server/config
+mkdir -p ./se-server/config/World
+mkdir -p ./se-server/config/Plugins
 
 docker run -it -v "steamcmd_volume:/home/steam/steamcmd" --name=steamcmd cm2network/steamcmd /bin/sh -c "./steamcmd.sh +force_install_dir ./bins +login anonymous +app_update 298740 +quit"
+# docker run -it --name=steamcmd cm2network/steamcmd /bin/sh -c "./steamcmd.sh +force_install_dir ./bins +login anonymous +app_update 298740 +quit"
 docker cp steamcmd:home/steam/steamcmd/bins/ ./se-server/
 
 cp ./source/SpaceEngineers-Dedicated.cfg ./se-server/config/SpaceEngineers-Dedicated.cfg
+cp -R ./source/World/ ./se-server/config/World
 
 docker rm steamcmd
