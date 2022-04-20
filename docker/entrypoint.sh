de4833d3a605f69b2a09cf655b781476fcb4940d
Jota Lanusse <jotalanusse@gmail.com>
2022-04-19 22:34:30 -0300

    Updated some bash scripts

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
 
-# # VARIABLES
-# GAME_DIR="/se-server/bins"
-# CONFIG_PATH="/se-server/config/SpaceEngineers-Dedicated.cfg"
-# INSTANCE_IP=$(hostname -I | sed "s= ==g")
-
-# # echo "-------------------------------INSTALL & UPDATE------------------------------"
-# # /usr/games/steamcmd +force_install_dir ${GAME_DIR} +login anonymous +@sSteamCmdForcePlatformType windows +app_update 298740 +quit
-
-# echo "---------------------------------UPDATE CONFIG-------------------------------"
-# # update IP to host external ip
-# CURRENT_IP=$(grep -oEi '<IP>(.*)</IP>' ${CONFIG_PATH} | sed "s=<IP>==g" | sed "s=</IP>==g")
-# sed -i "s=<IP>.*</IP>=<IP>${INSTANCE_IP}</IP>=g" ${CONFIG_PATH}
-
-# # update world save path
-# CURRENT_WORLDNAME=$(grep -oEi '<WorldName>(.*)</WorldName>' ${CONFIG_PATH} | sed "s=<WorldName>==g" | sed "s=</WorldName>==g")
-# SAVE_PATH="Z:\\\\appdata\\\\space-engineers\\\\instances\\\\${INSTANCE_NAME}\\\\Saves\\\\${CURRENT_WORLDNAME}";
-# sed -i "s=<LoadWorld>.*</LoadWorld>=<LoadWorld>${SAVE_PATH}</LoadWorld>=g" ${CONFIG_PATH}
-
-# echo "-----------------------------CURRENT CONFIGURATION---------------------------"
-# echo "GAME_DIR=$GAME_DIR"
-# echo "CONFIG_PATH=$CONFIG_PATH"
-# echo "INSTANCE_IP=$INSTANCE_IP"
-# echo "CURRENT_IP=$CURRENT_IP"
-# echo "CURRENT_WORLDNAME=$CURRENT_WORLDNAME"
-# echo "SAVE_PATH=$SAVE_PATH"
-
-## END UPDATES ##
+# Print the wine version for the meme
+echo ">> Wine version:"
 wine --version
-echo "----------------------------------START GAME---------------------------------"
+
+# Start the server using wine
+echo ">> STARTING SERVER"
 cd /se-server/bins/DedicatedServer64/
-wine SpaceEngineersDedicated.exe -noconsole -ignorelastsession -path Z:\\se-server\\config
-echo "-----------------------------------END GAME----------------------------------"
+wine SpaceEngineersDedicated.exe -noconsole -path Z:\\se-server\\config
+echo ">> CLOSING SERVER"
+
+# Some timeout for ux
 sleep 10
-echo "-----------------------------------BYE !!!!----------------------------------"
+echo ">> SRVER CLOSE"
